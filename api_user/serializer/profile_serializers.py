07950e24a0658a4b45df50cd11b379b5071031e1
khoa <quockhoa1911@gmail.com>
2022-10-20 22:06:30 +0700

    update serializer

--- api_user/serializer/profile_serializers.py
+++ api_user/serializer/profile_serializers.py
@@ -1,16 +1,15 @@
 from rest_framework import serializers
 from ..models import Profiles
-from ..serializer import Payment_type_serializers
+from ..serializer import Profile_Payment_type_serializers
 
 class Profile_serializers(serializers.ModelSerializer):
-    payment_type = Payment_type_serializers(many=True,read_only=True)
+    profile_payment_type = Profile_Payment_type_serializers(many=True,read_only=True)
     class Meta:
         model = Profiles
-        fields = ['id','name','gender','age','phone_number','email','certificate','avatar','payment_type','province',
+        fields = ['id','name','gender','age','phone_number','email','certificate','avatar','profile_payment_type','province',
                   'province_code','ward','ward_code','district','district_code','address']
 
 class Profile_write_serializers(serializers.ModelSerializer):
-
     class Meta:
         model = Profiles
         fields = ['email','certificate']
--- api_user/serializer/role_serializers.py
+++ api_user/serializer/role_serializers.py
@@ -1,12 +1,10 @@
 from rest_framework import serializers
-from ..serializer import Account_Role_serializers
 from ..models import Roles
 
 class Role_serializers(serializers.ModelSerializer):
-    # status = Account_Role_serializers(many=True,read_only=True,source="account_role")
     #if name field the same related name then no source else required source
-    status = serializers.SlugRelatedField(source="account_role",read_only=True,many=True,slug_field="is_active")
+    # status = serializers.SlugRelatedField(source="account_role",read_only=True,many=True,slug_field="is_active")
     # get one more fields use slug else get two or than  use Serializer model
     class Meta:
         model = Roles
-        fields = ['id','name','status']
+        fields = ['id','name']
